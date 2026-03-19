12883cd9307caa0a6994a8120f12fe14dfbe1be6
TheMoon <woghd1008@gmail.com>
2026-03-19 22:13:54 +0900

    2026-03-19 Java Enum

---
+++ src/enumeration/ex2/ClassGradeEx2_1.java
@@ -0,0 +1,19 @@
+package enumeration.ex2;
+
+import enumeration.ex1.DiscountService;
+import enumeration.ex1.StringGrade;
+
+public class ClassGradeEx2_1 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+        DiscountService discountService = new DiscountService();
+        int basic = discountService.discount(StringGrade.BASIC, price);
+        int gold = discountService.discount(StringGrade.GOLD, price);
+        int diamond = discountService.discount(StringGrade.DIAMOND, price);
+
+        System.out.println("BASIC 등급의 할인 금애: " + basic);
+        System.out.println("GOLD 등급의 할인 금애: " + gold);
+        System.out.println("DIAMOND 등급의 할인 금애: " + diamond);
+    }
+}
