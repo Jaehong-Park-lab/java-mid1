979745a384ee7bb731a891630a5e6456eeecc085
TheMoon <woghd1008@gmail.com>
2026-04-03 23:00:54 +0900

    2026-04-03 Java Exception Part 2

---
+++ src/exception/ex4/NetworkServiceV4.java
@@ -0,0 +1,21 @@
+package exception.ex4;
+
+
+public class NetworkServiceV4 {
+
+    public void sendMessage(String data) {
+        String address = "http://example.com";
+        NetworkClientV4 client = new NetworkClientV4(address);
+        client.initError(data); //추가
+
+        try {
+            client.connect();
+            client.send(data); //SendExceptionV3
+        } finally {
+            client.disconnect();
+
+        }
+
+
+    }
+}
